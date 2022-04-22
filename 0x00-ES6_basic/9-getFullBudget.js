d41e43e6a5b73b0ef82e3518b1b43c6091b34b59
Tosin Adewale <84603888+Tosmel2@users.noreply.github.com>
2022-04-22 19:34:09 +0100

    ES6 method properties

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -5,7 +5,7 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const fullBudget = {
     ...budget,
     getIncomeInDollars(income) {
-      return `${income}`;
+      return `$${income}`;
     },
     getIncomeInEuros(income) {
       return `${income} euros`;
